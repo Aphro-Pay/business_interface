b21463700b182671d3c871b4c745c91c6e159a89
BusayoSamuel <busayosalisu@gmail.com>
2024-06-12 23:04:33 +0100

    initial set-up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="./assets/images/aphro_logo.png"
+          className="App-logo"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;

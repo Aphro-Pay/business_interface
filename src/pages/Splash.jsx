225d281744068c232ac72f3297837b6a01c10356
BusayoSamuel <busayosalisu@gmail.com>
2025-01-26 06:53:56 +0000

    fix: overlay effect in page transitions - wrap page content in <IonContent>

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import RoundButton from "../components/RoundButton";
 import Space from "../components/Space";
 import styles from "./Splash.module.css";
-import { IonPage } from "@ionic/react";
+import { IonPage, IonContent } from "@ionic/react";
 
 function Splash() {
   return (
     <IonPage>
-      <div className="scaffold">
-        <div className={styles.body}>
-          <div className={styles.content}>
-            <img
-              src="./assets/images/aphro_pay_logo.jpg"
-              alt="logo"
-              className={styles.img}
-            />
-            <Space height="16px" />
-            <RoundButton text="Log in" navigateTo="/login" />
-            <Space height="16px" />
-            <RoundButton
-              text="Sign up"
-              className="grey"
-              navigateTo="/create_your_login_details"
-            />
+      <IonContent>
+        <div className="scaffold">
+          <div className={styles.body}>
+            <div className={styles.content}>
+              <img
+                src="./assets/images/aphro_pay_logo.jpg"
+                alt="logo"
+                className={styles.img}
+              />
+              <Space height="16px" />
+              <RoundButton text="Log in" navigateTo="/login" />
+              <Space height="16px" />
+              <RoundButton
+                text="Sign up"
+                className="grey"
+                navigateTo="/create_your_login_details"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </IonContent>
     </IonPage>
   );
 }
--- src/pages/authentication/log_in/LogIn.jsx
+++ src/pages/authentication/log_in/LogIn.jsx
@@ -5,7 +5,7 @@ import RoundButton from "../../../components/RoundButton";
 import Input from "../../../components/Input";
 import ClickableText from "../../../components/ClickableText";
 import Space from "../../../components/Space";
-import { IonPage } from "@ionic/react";
+import { IonPage, IonContent } from "@ionic/react";
 import {
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
@@ -79,55 +79,57 @@ function LogIn() {
 
   return (
     <IonPage>
-      <div className={styles.scaffold}>
-        <Header enableBackButton="n" />
-        <div className={styles.title}>Log in to your account</div>
-        <Space height="50px" />
-        <div className={styles.body}>
-          <form className={`${styles.column} ${styles.content}`}>
-            <Input
-              hintText="john.doe@gmail.com"
-              label="Email"
-              onChange={handleEmailChange}
-            />
+      <IonContent>
+        <div className={styles.scaffold}>
+          <Header enableBackButton="n" />
+          <div className={styles.title}>Log in to your account</div>
+          <Space height="50px" />
+          <div className={styles.body}>
+            <form className={`${styles.column} ${styles.content}`}>
+              <Input
+                hintText="john.doe@gmail.com"
+                label="Email"
+                onChange={handleEmailChange}
+              />
+              <Space height="15px" />
+              <Input
+                hintText="********"
+                label="Password"
+                onChange={handlePasswordChange}
+              />
+              <ClickableText
+                text="Forgot Password?"
+                onClick={async () => {
+                  if (email === "") {
+                    alert("Please enter your email");
+                  } else {
+                    await sendPasswordResetEmail(auth, email);
+                    alert("Please check your inbox to reset your password");
+                  }
+                }}
+              />
+              <Space height="15px" />
+              <RoundButton
+                text="Log in"
+                onClick={SignIn}
+                //navigateTo="/enter_mobile_number"
+              />
+            </form>
             <Space height="15px" />
-            <Input
-              hintText="********"
-              label="Password"
-              onChange={handlePasswordChange}
-            />
-            <ClickableText
-              text="Forgot Password?"
-              onClick={async () => {
-                if (email === "") {
-                  alert("Please enter your email");
-                } else {
-                  await sendPasswordResetEmail(auth, email);
-                  alert("Please check your inbox to reset your password");
-                }
-              }}
-            />
-            <Space height="15px" />
-            <RoundButton
-              text="Log in"
-              onClick={SignIn}
-              //navigateTo="/enter_mobile_number"
-            />
-          </form>
-          <Space height="15px" />
-          <div className={`${styles.row} ${styles.content}`}>
-            <span>Don't have an account?</span>
-            <Space width="5px" />
-            <ClickableText
-              text="Sign Up"
-              textDecoration="underline"
-              onClick={() => {
-                history.push("/create_your_login_details");
-              }}
-            />
+            <div className={`${styles.row} ${styles.content}`}>
+              <span>Don't have an account?</span>
+              <Space width="5px" />
+              <ClickableText
+                text="Sign Up"
+                textDecoration="underline"
+                onClick={() => {
+                  history.push("/create_your_login_details");
+                }}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </IonContent>
     </IonPage>
   );
 }
